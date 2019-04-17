7927fa2fa6020c70ced9901e853fa97d4a5cf87e
mhashim6 <m6hashim@gmail.com>
2019-04-17 09:40:08 +0200

    problem parameters are no longer hardcoded

--- bankers_algorithm/bankers_algorithm.c
+++ bankers_algorithm/bankers_algorithm.c
@@ -4,51 +4,77 @@ compiled against std=gnu89 to support the LC-3 C toolchain.
 
 #include <stdio.h>
 
-#define N 5 /*Number of processes*/
-#define M 3 /*Number of resources*/
+#define N 20 /*Maximum Number of processes*/
+#define M 20 /*Number of resources*/
 
 int main()
 {
-    int allocs[N][M] =
-        {
-            {0, 1, 0}, /*P0*/
-            {2, 0, 0}, /*P1*/
-            {3, 0, 2}, /*P2*/
-            {2, 1, 1}, /*P3*/
-            {0, 0, 2}  /*P4*/
-        };
+    int allocs[N][M];
 
-    int max[N][M] =
-        {
-            {7, 5, 3}, /*P0*/
-            {3, 2, 2}, /*P1*/
-            {9, 0, 2}, /*P2*/
-            {2, 2, 2}, /*P3*/
-            {4, 3, 3}  /*P4*/
-        };
+    int max[N][M];
 
-    int work[M] = {3, 3, 2} /* initially equals available.*/;
+    int work[M];
 
     int need[N][M], finish[N], seq[N];
-
+    int n, m = 0;                /*actual values of n and m, defined by user input.*/
     int i, j, k, y, seq_ind = 0; /*indicies*/
+    int safe = 1;
 
-    for (k = 0; k < N; k++)
+    printf("Enter number of processes, maximum %d: ", N);
+    scanf("%d", &n);
+    printf("\n");
+
+    printf("Enter number of resources, maximum %d: ", M);
+    scanf("%d", &m);
+    printf("\n");
+
+    printf("Enter allocations, row by row: ");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+            scanf("%d", &allocs[i][j]);
+    printf("\n");
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+            printf("%d ", allocs[i][j]);
+        printf("\n");
+    }
+
+    printf("Enter max, row by row: ");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+            scanf("%d", &max[i][j]);
+    printf("\n");
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+            printf("%d ", max[i][j]);
+        printf("\n");
+    }
+
+    printf("Enter available resources: ");
+    for (i = 0; i < m; i++)
+        scanf("%d", &work[i]); /* initially equals available.*/
+    printf("\n");
+
+    for (k = 0; k < n; k++)
         finish[k] = 0;
 
     /*Need = Max - Allocations*/
-    for (i = 0; i < N; i++)
-        for (j = 0; j < M; j++)
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
             need[i][j] = max[i][j] - allocs[i][j];
 
-    for (k = 0; k < N; k++)
+    for (k = 0; k < n; k++)
     {
-        for (i = 0; i < N; i++)
+        for (i = 0; i < n; i++)
         {
             if (!finish[i])
             {
                 int illegal = 0;
-                for (j = 0; j < M; j++)
+                for (j = 0; j < m; j++)
                 {
                     if (need[i][j] > work[j])
                     {
@@ -61,7 +87,7 @@ int main()
                 {
                     seq[seq_ind++] = i;
                     /*Work = Work + Allocations*/
-                    for (y = 0; y < M; y++)
+                    for (y = 0; y < m; y++)
                         work[y] += allocs[i][y];
                     finish[i] = 1;
                 }
@@ -70,9 +96,9 @@ int main()
     }
 
     printf("Safe sequence:\n");
-    for (i = 0; i < N - 1; i++)
+    for (i = 0; i < n - 1; i++)
         printf("P%d -> ", seq[i]);
-    printf("P%d\n", seq[N - 1]);
+    printf("P%d\n", seq[n - 1]);
 
-    return (0);
+    return 0;
 }
